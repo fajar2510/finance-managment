ba4cbac40a249442b2bb9c62ef9b3d4cf0735eb3
Fajar Abdurrohman <sepertiseharusnyafajar@gmail.com>
2025-01-20 10:14:03 +0700

    webpack issue applied config for it

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+ trailingComma: 'es5',
+ tabWidth: 4,
+ semi: false,
+ singleQuote: true,
+ requireConfig: true,
+ bracketSpacing: true,
+ bracketSameLine: false,
+ jsxBracketSameLine: false,
+ singleAttributePerLine: true,
+}
